4f0121a92fc0866553ee3154654dcbc93172f2d1
HR <751572781@qq.com>
2017-07-05 23:03:03 +0800

    map、泛型

---
+++ src/demo/Demo2.java
@@ -0,0 +1,16 @@
+package demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by HR on 2017/7/5.
+ * 泛型
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        list.add("11");
+        list.get(0);
+    }
+}
--- src/demo/demo1.java
+++ src/demo/demo1.java
@@ -5,34 +5,39 @@ import java.lang.reflect.InvocationTargetException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.beanutils.ConversionException;
 import org.apache.commons.beanutils.ConvertUtils;
 import org.apache.commons.beanutils.Converter;
+import org.apache.commons.beanutils.converters.DateConverter;
+import org.apache.commons.beanutils.locale.converters.DateLocaleConverter;
 import org.junit.Test;
 
-public class demo1{
+public class demo1 {
     @Test
     public void test1() throws IllegalAccessException, InvocationTargetException {
-        Person p= new Person();
-        String name="张三";
-        String password="sssaaa";
-        String age ="22";
-        String birthday="1990-01-01";
-        int b=123;
+        Person p = new Person();
+        String name = "张三";
+        String password = "sssaaa";
+        String age = "22";
+        String birthday = "1990-01-01";
+        int b = 123;
+        //beanutils只支持基础数据的类型转换，如果需要支持别的类型，使用下面的注册转换器
         ConvertUtils.register(new Converter() {
 
             @Override
             public Object convert(Class arg0, Object arg1) {
-                if(arg1==null) {
+                if (arg1 == null) {
                     return null;
                 }
-                if(!(arg1 instanceof String)) {
+                if (!(arg1 instanceof String)) {
                     throw new ConversionException("不是String类型");
                 }
-                String str= (String) arg1;
-                if(str.trim().equals("")) {
+                String str = (String) arg1;
+                if (str.trim().equals("")) {
                     return null;
                 }
                 SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
@@ -53,9 +58,47 @@ public class demo1{
         System.out.println(p.getName());
         System.out.println(p.getPassword());
         System.out.println(p.getAge());
-        System.out.println(p.getBirthday());
-        System.out.println("");
+        //System.out.println(p.getBirthday());
+        //System.out.println("");
+        Date date = p.getBirthday();
+        System.out.println(date.toInstant());//java8新特性
     }
 
+    @Test
+    public void test2() throws IllegalAccessException, InvocationTargetException {
+        Person p = new Person();
+        String name = "张三";
+        String password = "sssaaa";
+        String age = "22";
+        String birthday = "1990-02-01";
+        int b = 123;
+        //beanutils已经实现的转换器
+        ConvertUtils.register(new DateLocaleConverter(), Date.class);
 
+        BeanUtils.setProperty(p, "name", name);
+        BeanUtils.setProperty(p, "password", password);
+        BeanUtils.setProperty(p, "age", age);
+        BeanUtils.setProperty(p, "birthday", birthday);
+
+        System.out.println(p.getName());
+        System.out.println(p.getPassword());
+        System.out.println(p.getAge());
+        System.out.println(p.getBirthday());
+    }
+    @Test
+    public void test3() throws InvocationTargetException, IllegalAccessException {
+        Map map=new HashMap();
+        map.put("name","lisi");
+        map.put("password","aassaa");
+        map.put("age","22");
+        map.put("birthday","1990-01-01");
+
+        ConvertUtils.register(new DateLocaleConverter(), Date.class);
+        Person bean = new Person();
+        BeanUtils.populate(bean,map);      //用map中的值，填充bean的属性
+        System.out.println(bean.getName());
+        System.out.println(bean.getPassword());
+        System.out.println(bean.getAge());
+        System.out.println(bean.getBirthday());
+    }
 }
