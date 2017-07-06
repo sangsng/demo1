09b06a21b5d5d86ddf769ef8104482d8e098fb42
HR <751572781@qq.com>
2017-07-06 23:38:07 +0800

    map、泛型

--- src/demo/Demo2.java
+++ src/demo/Demo2.java
@@ -33,11 +33,15 @@ public class Demo2 {
 
     @Test
     public void test1() {
-        Map<Integer, String> map = new HashMap<>();
-
-        map.put(1, "aa");
+        // HashMap是没有顺序的，而且读取的时候也不一定按照你存的顺序输出，它按照哈希值存储的
+        //LinkedHashMap有序，输出时保持数据存入时的顺序
+        Map<Integer, String> map = new LinkedHashMap<Integer,String>();
         map.put(2, "bb");
+        map.put(1, "aa");
+
         map.put(3, "cc");
+        map.put(4, "dd");
+        map.put(5, "ee");
 
         Set<Map.Entry<Integer, String>> set = map.entrySet();
         for (Map.Entry entry : set
@@ -45,6 +49,10 @@ public class Demo2 {
             System.out.println("key: " + entry.getKey() + " value: " + entry.getValue());
         }
         Iterator<Map.Entry<Integer, String>> it = set.iterator();
+        while (it.hasNext()){
+            Map.Entry<Integer, String> st=it.next();
+            System.out.println(st.getKey() + " = " + st.getValue());
+        }
 
     }
 
@@ -52,6 +60,7 @@ public class Demo2 {
     public void test2() {
         int i = 1;
         int j = i++;
+        //i++是在语句运行后进行+=计算，++i是先允许+=计算其他语句。
         if ((i == (++j)) && ((i++) == j)) {
             i += j;
         }
