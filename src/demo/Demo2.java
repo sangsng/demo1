dbf074da5fea24be4f8764a0adf49f2283373521
HR <751572781@qq.com>
2017-07-06 12:49:20 +0800

    map、泛型

--- src/demo/Demo2.java
+++ src/demo/Demo2.java
@@ -1,9 +1,9 @@
 package demo;
 
+import org.junit.Test;
+
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by HR on 2017/7/5.
@@ -16,18 +16,45 @@ public class Demo2 {
         list.add("bb");
         list.add("cc");
 
-        Iterator<String> it= list.iterator();
-        while(it.hasNext()){
-            String str=it.next();
+        Iterator<String> it = list.iterator();
+        while (it.hasNext()) {
+            String str = it.next();
             System.out.println(str);
         }
-        for (String a:list
-             ) {
+        for (String a : list
+                ) {
             System.out.println(a);
-            
+
         }
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
+    }
+
+    @Test
+    public void test1() {
+        Map<Integer, String> map = new HashMap<>();
+
+        map.put(1, "aa");
+        map.put(2, "bb");
+        map.put(3, "cc");
+
+        Set<Map.Entry<Integer, String>> set = map.entrySet();
+        for (Map.Entry entry : set
+                ) {
+            System.out.println("key: " + entry.getKey() + " value: " + entry.getValue());
+        }
+        Iterator<Map.Entry<Integer, String>> it = set.iterator();
+
+    }
+
+    @Test
+    public void test2() {
+        int i = 1;
+        int j = i++;
+        if ((i == (++j)) && ((i++) == j)) {
+            i += j;
+        }
+        System.out.println("i = " + i);
     }
 }
